0c7993daaaefea4f0191a6e745a61c8dcbc72627
Francesco C <fran461j@outlook.it>
2023-01-17 21:46:44 +0100

    trains in databese displayed in homepage

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,12 +1,13 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Train;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index(){
-        return view('home');
+        $trains = Train::all();
+        return view('home', compact('trains'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,33 @@
 @section('title', 'Trenitalia')
 @section('main')
     <main>
-        
+        @foreach ($trains as $item)
+            <h3>
+                codice di serie: 
+                {{ $item['serial_code']}}
+            </h3>   
+            <h3>
+                regione: 
+                {{ $item['operating_region']}}
+            </h3>       
+            <h3>
+                data messa in servizio: 
+                {{ $item['working_start_date']}}
+            </h3>       
+            <h3>
+                ultima manutenzione: 
+                {{ $item['last_mainteinance']}}
+            </h3>       
+            <h3>
+                prossima manutenzione: 
+                {{ $item['next_mainteinance']}}
+            </h3>       
+            <h3>
+                data di ritiro: 
+                {{ $item['retirement_date']}}
+            </h3>       
+
+
+        @endforeach
     </main>
 @endsection
