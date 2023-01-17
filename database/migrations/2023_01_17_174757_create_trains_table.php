da64e8e9ff1185e13714f3a3b439655344912c3d
Francesco C <fran461j@outlook.it>
2023-01-17 20:44:37 +0100

    migrated db table

---
+++ database/migrations/2023_01_17_174757_create_trains_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTrainsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->mediumInteger('serial_code');
+            $table->string('operating_region');
+            $table->date('working_start_date');
+            $table->date('last_mainteinance');
+            $table->date('next_mainteinance');
+            $table->date('retirement_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+}
---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layout.app')
+@section('title', 'Trenitalia')
+@section('main')
+    <main>
+        
+    </main>
+@endsection
